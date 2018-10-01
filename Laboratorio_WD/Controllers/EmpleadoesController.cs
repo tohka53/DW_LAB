2943b007dd5626bc6d8c682bac9acd58fe483871
tohka53 <mecg1994@gmail.com>
2018-09-30 21:12:03 -0600

    CRUD COMPLETO

---
+++ Laboratorio_WD/Controllers/EmpleadoesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Laboratorio_WD.Models;
+using System.Web.Http.Cors;
+
+namespace Laboratorio_WD.Controllers
+{
+    //Se coloca en el web api principal
+   // [EnableCors(origins: "http://localhost:4216", headers: "*", methods: "*")]
+    public class EmpleadoesController : ApiController
+    {
+        private Laboratorio_DWEntities db = new Laboratorio_DWEntities();
+
+        // GET: api/Empleadoes
+        public IQueryable<Empleado> GetEmpleados()
+        {
+            return db.Empleados;
+        }
+
+        // GET: api/Empleadoes/5
+        [ResponseType(typeof(Empleado))]
+        public IHttpActionResult GetEmpleado(int id)
+        {
+            Empleado empleado = db.Empleados.Find(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(empleado);
+        }
+
+        // PUT: api/Empleadoes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmpleado(int id, Empleado empleado)
+        {
+      
+
+            if (id != empleado.id_empleado)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(empleado).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpleadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Empleadoes
+        [ResponseType(typeof(Empleado))]
+        public IHttpActionResult PostEmpleado(Empleado empleado)
+        {
+       
+
+            db.Empleados.Add(empleado);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = empleado.id_empleado }, empleado);
+        }
+
+        // DELETE: api/Empleadoes/5
+        [ResponseType(typeof(Empleado))]
+        public IHttpActionResult DeleteEmpleado(int id)
+        {
+            Empleado empleado = db.Empleados.Find(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            db.Empleados.Remove(empleado);
+            db.SaveChanges();
+
+            return Ok(empleado);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+            return db.Empleados.Count(e => e.id_empleado == id) > 0;
+        }
+    }
+}
